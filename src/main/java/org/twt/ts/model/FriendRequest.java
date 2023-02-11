6047c313404a63b2af4fb12ec2220ddfa6e741d1
steven12138 <18611209860@163.com>
2023-02-11 19:42:25 +0800

    finish friend request database design

--- src/main/java/org/twt/ts/model/FriendRequest.java
+++ src/main/java/org/twt/ts/model/FriendRequest.java
@@ -10,5 +10,18 @@ public class FriendRequest {
     @Column(name = "rid")
     private int rid;
 
+    @JoinColumn(name = "request_from")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Account from;
+
+    @JoinColumn(name = "request_to")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Account to;
+
+    @Column(name = "description")
+    private String desc;
+
+    @Column(name = "accept")
+    private boolean accept = false;
 
 }
